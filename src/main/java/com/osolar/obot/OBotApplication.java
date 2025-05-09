5fd324e796d6759e3d470d7dc6731c59b498916a
minahkim03 <minahkim026@gmail.com>
2025-05-09 20:14:38 +0900

    [Initial] 초기 세팅

---
+++ src/main/java/com/osolar/obot/OBotApplication.java
@@ -0,0 +1,13 @@
+package com.osolar.obot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/osolar/obot/OBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osolar.obot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
