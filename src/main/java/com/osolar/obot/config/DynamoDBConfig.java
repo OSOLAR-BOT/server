c47f0ffc19e023ce26946c7bc333e4ce1e4e0458
dvlp-sy <sy8389b@gmail.com>
2025-05-11 17:34:55 +0900

    [ADD] Create DynamoDBConfig

---
+++ src/main/java/com/osolar/obot/config/DynamoDBConfig.java
@@ -0,0 +1,53 @@
+package com.osolar.obot.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DynamoDBConfig {
+
+    @Value("${aws.dynamodb.endpoint}")
+    private String endPoint;
+
+    @Value("${aws.region}")
+    private String region;
+
+    @Value("${aws.access-key}")
+    private String accessKey;
+
+    @Value("${aws.secret-key}")
+    private String secretKey;
+
+
+    @Bean(name = "dynamoDBMapper")
+    public DynamoDBMapper dynamoDBMapper() {
+        DynamoDBMapperConfig config = DynamoDBMapperConfig.builder()
+                .withSaveBehavior(DynamoDBMapperConfig.SaveBehavior.CLOBBER)
+                .withConsistentReads(DynamoDBMapperConfig.ConsistentReads.CONSISTENT)
+                .withTableNameOverride(null)
+                .withPaginationLoadingStrategy(DynamoDBMapperConfig.PaginationLoadingStrategy.EAGER_LOADING)
+                .build();
+        return new DynamoDBMapper(amazonDynamoDB(), config);
+    }
+
+    @Bean(name = "amazonDynamoDB")
+    public AmazonDynamoDB amazonDynamoDB() {
+        AWSStaticCredentialsProvider credentialsProvider = new AWSStaticCredentialsProvider(
+                new BasicAWSCredentials(accessKey, secretKey));
+        EndpointConfiguration endpointConfiguration =
+                new EndpointConfiguration(endPoint, region);
+
+        return AmazonDynamoDBClientBuilder.standard()
+                .withCredentials(credentialsProvider)
+                .withEndpointConfiguration(endpointConfiguration)
+                .build();
+    }
+}
