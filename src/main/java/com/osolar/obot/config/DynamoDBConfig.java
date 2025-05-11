93e9766370c264d327a51d6ceb01268dbf3ea11e
dvlp-sy <sy8389b@gmail.com>
2025-05-11 21:23:44 +0900

    [ADD] Add LocalDateTime Converter

--- src/main/java/com/osolar/obot/config/DynamoDBConfig.java
+++ src/main/java/com/osolar/obot/config/DynamoDBConfig.java
@@ -7,9 +7,15 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTypeConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+import java.util.TimeZone;
 
 @Configuration
 public class DynamoDBConfig {
@@ -25,7 +31,6 @@ public class DynamoDBConfig {
 
     @Value("${aws.secret-key}")
     private String secretKey;
-
 
     @Bean(name = "dynamoDBMapper")
     public DynamoDBMapper dynamoDBMapper() {
@@ -50,4 +55,18 @@ public class DynamoDBConfig {
                 .withEndpointConfiguration(endpointConfiguration)
                 .build();
     }
+
+    public static class LocalDateTimeConverter implements DynamoDBTypeConverter<Date, LocalDateTime> {
+
+        @Override
+        public Date convert(LocalDateTime source) {
+            return Date.from(source.toInstant(ZoneOffset.UTC));
+        }
+
+        @Override
+        public LocalDateTime unconvert(Date source) {
+            return source.toInstant().atZone(TimeZone.getDefault().toZoneId()).toLocalDateTime();
+        }
+    }
+
 }
